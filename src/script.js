ded1cdc3840a99061f34bc0cf031e2ad5173cb71
Barbara Peric <b.peric10@gmail.com>
2024-04-03 10:48:04 +0200

    FIX

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-// Mouse
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+// Cursor
 const cursor = {
   x: 0,
   y: 0,
@@ -13,59 +24,57 @@ window.addEventListener('mousemove', (event) => {
   cursor.y = -(event.clientY / sizes.height - 0.5)
 })
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.y = 0
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
 scene.add(mesh)
 
-/**
- * Sizes
- */
-const sizes = {
-  width: 800,
-  height: 600,
-}
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
 
-/**
- * Renderer
- */
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// ANIMATION
-
+// Animate
 const clock = new THREE.Clock()
 
-// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2})
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-function tick() {
-  //const elapsedTime = clock.getElapsedTime()
-  //mesh.position.y =  Math.sin(elapsedTime)
-  //mesh.position.x =  Math.cos(elapsedTime)
-  camera.position.x = cursor.x * 3
-  camera.position.y = cursor.y * 3
+  // Update controls
+  controls.update()
+
+  // Update camera
+  camera.position.x = cursor.x * 5
+  camera.position.y = cursor.y * 5
+  camera.lookAt(mesh.position)
+
+  // Render
   renderer.render(scene, camera)
 
-  //   window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
